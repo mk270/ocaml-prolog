6a724a98f40e279d5690d1272b56cdef93e3533a
Martin Keegan <martin@no.ucant.org>
2012-11-06 14:57:39 +0000

    break main repl loop down to idiomatic ocaml

--- src/opl.ml
+++ src/opl.ml
@@ -47,19 +47,33 @@ let read_database params =
 				raise e
     in
     begin
-        let parameters = match Array.length params with              (* the first argument of the program is it's name *)
-                            |  1  -> Array.make 0 ""                 (* omit the first parameter - case it's the only one *)
-                            |  i  -> Array.sub params 1 (i - 1)      (* omit the first parameter - case of more than 1 parameter *)
+        let parameters = match Array.length params with
+            |  1  -> Array.make 0 ""
+            |  i  -> Array.sub params 1 (i - 1)
         in
-            Array.iter (fun filename -> if Sys.file_exists filename         (* check if the file in param exists in filesystem *)
-                                        then extend_database filename       (* try to extend database with file contents *)
-                                        else print_endline                  (* warn user *)
-                                                ("File " ^ (Filename.basename filename) ^ " does not exist."))   
-                        parameters;
-            !database                                           (* return created database *)
+            Array.iter (fun filename -> 
+				if Sys.file_exists filename
+                then extend_database filename
+                else print_endline ("File " ^ (Filename.basename filename) ^ " does not exist.")) parameters;
+            !database
     end
 
+let prompt () =
+	print_string ":- "; 
+	flush stdout
 
+let read_eval_print database =
+	prompt ();
+
+    try	let db = shuffle database
+	    and buff = Lexing.from_string (read_line ())
+		in
+			interpret (Parser.query Lexer.token buff) db false false;
+    with
+        | Failure ("lexing: empty token")    (* lexing failure *)
+        | Parsing.Parse_error ->             (* parsing failure *)
+            print_endline "Parse error. Did you forget a dot?"
+        | Failure s -> print_endline ("Failed: " ^ s) 
 
 (*
  *  main interpreter function.
@@ -69,29 +83,15 @@ let main () =
     let database = read_database Sys.argv
     in 
     	try 
-			while (true) 				(* while not EOF *)
-			do	
-	    		print_string ":- ";
-                flush stdout;
-
-                try
-					let db = shuffle database
-						(* read the expression *)
-					and	buff = Lexing.from_string(read_line())
-						(* create its syntax tree*)
-					in
-						interpret (Parser.query Lexer.token buff) db false false;
-                with
-                    | Failure ("lexing: empty token")    (* lexing failure *)
-                    | Parsing.Parse_error ->             (* parsing failure *)
-                        print_endline "Parse error. Did you forget a dot?"
-                    | Failure s -> print_endline ("Failed: " ^ s) 
-			done
+			let rec main_loop_body () =
+				read_eval_print database; (* exception on EOF *)
+				main_loop_body ()
+			in 
+				main_loop_body ()
 		with
 			| End_of_file -> (print_string "\n"; exit 0)
-            |      _      -> (print_endline "Error occured."; exit 0)
-				
-				
+            | _           -> (print_endline "Error occurred."; exit 0)
+
 let _ = 
 	Random.self_init ();
 	main ()  
