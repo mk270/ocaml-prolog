303e0cc5d4214a6167033729303e66ae6bde0621
Martin Keegan <martin@no.ucant.org>
2013-06-28 00:34:07 +0100

    factor cases together

--- src/unificator.ml
+++ src/unificator.ml
@@ -30,31 +30,32 @@ let string_of_term term =
 						| Integer n -> string_of_int n))
 		| TermFunctor(nam,args) ->
 			nam ^ "(" ^ (string_of_arguments args) ^ ")"
-		| TermIfThen (t1, t2) -> "if " ^ (string_of_term t1) ^ " then " ^ (string_of_term t2)
-		| TermIfThenElse (t1, t2,t3) -> "if " ^ (string_of_term t1) ^ " then " ^ (string_of_term t2) ^ " else " ^ (string_of_term t3)
+		| TermIfThen (t1, t2) -> "if " ^ (string_of_terms_with_sep " then " t1 t2)
+		| TermIfThenElse (t1, t2,t3) -> "if " ^ (string_of_terms_with_sep " then " t1 t2) ^ " else " ^ (string_of_term t3)
 		| TermNegation t -> "not " ^ (string_of_term t)
-		| TermIs (t1, t2) -> (string_of_term t1) ^ " is " ^ (string_of_term t2)
-		| TermOr (t1, t2) -> (string_of_term t1) ^ "; " ^ (string_of_term t2)
-		| TermAnd (t1, t2) -> (string_of_term t1) ^ ", " ^ (string_of_term t2)
-		| TermArithmeticPlus (t1, t2) -> (string_of_term t1) ^ " + " ^ (string_of_term t2)
-		| TermArithmeticMinus (t1, t2) -> (string_of_term t1) ^ " - " ^ (string_of_term t2)
-		| TermArithmeticMult (t1, t2) -> (string_of_term t1) ^ " * " ^ (string_of_term t2)
-		| TermArithmeticDiv (t1, t2) -> (string_of_term t1) ^ " / " ^ (string_of_term t2)
-		| TermArithmeticEquality (t1, t2) -> (string_of_term t1) ^ " =:= " ^ (string_of_term t2)
-		| TermArithmeticInequality (t1, t2) -> (string_of_term t1) ^ " =\\= " ^ (string_of_term t2)
-		| TermArithmeticLess (t1, t2) -> (string_of_term t1) ^ " < " ^ (string_of_term t2)
-		| TermArithmeticGreater (t1, t2) -> (string_of_term t1) ^ " > " ^ (string_of_term t2)
-		| TermArithmeticLeq (t1, t2) -> (string_of_term t1) ^ " <= " ^ (string_of_term t2)
-		| TermArithmeticGeq (t1, t2) -> (string_of_term t1) ^ " >= " ^ (string_of_term t2)
-		| TermTermEquality (t1, t2) -> (string_of_term t1) ^ " == " ^ (string_of_term t2)
-		| TermTermUnify (t1, t2) -> (string_of_term t1) ^ " = " ^ (string_of_term t2)
-		| TermTermNotUnify (t1, t2) -> (string_of_term t1) ^ " /= " ^ (string_of_term t2)
+		| TermIs (t1, t2) -> (string_of_terms_with_sep " is " t1 t2)
+		| TermOr (t1, t2) -> (string_of_terms_with_sep "; " t1 t2)
+		| TermAnd (t1, t2) -> (string_of_terms_with_sep ", " t1 t2)
+		| TermArithmeticPlus (t1, t2) -> (string_of_terms_with_sep " + " t1 t2)
+		| TermArithmeticMinus (t1, t2) -> (string_of_terms_with_sep " - " t1 t2)
+		| TermArithmeticMult (t1, t2) -> (string_of_terms_with_sep " * " t1 t2)
+		| TermArithmeticDiv (t1, t2) -> (string_of_terms_with_sep " / " t1 t2)
+		| TermArithmeticEquality (t1, t2) -> (string_of_terms_with_sep " =:= " t1 t2)
+		| TermArithmeticInequality (t1, t2) -> (string_of_terms_with_sep " =\\= " t1 t2)
+		| TermArithmeticLess (t1, t2) -> (string_of_terms_with_sep " < " t1 t2)
+		| TermArithmeticGreater (t1, t2) -> (string_of_terms_with_sep " > " t1 t2)
+		| TermArithmeticLeq (t1, t2) -> (string_of_terms_with_sep " <= " t1 t2)
+		| TermArithmeticGeq (t1, t2) -> (string_of_terms_with_sep " >= " t1 t2)
+		| TermTermEquality (t1, t2) -> (string_of_terms_with_sep " == " t1 t2)
+		| TermTermUnify (t1, t2) -> (string_of_terms_with_sep " = " t1 t2)
+		| TermTermNotUnify (t1, t2) -> (string_of_terms_with_sep " /= " t1 t2)
 		| TermList list ->
 			(match list with
 				| EmptyList -> "[]"
 				| NormalList args -> "[" ^ (string_of_arguments args) ^ "]"
 				| DividedList(args,term) -> "[" ^ (string_of_arguments args) ^ " | " ^ (string_of_term term) ^ "]")
 		| _ -> ""
+	and string_of_terms_with_sep sep t1 t2 = (string_of_term t1) ^ " " ^ sep ^ " " ^ (string_of_term t2)
 	in
 		string_of_term term
 
